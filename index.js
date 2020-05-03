c7f7dbaa9b4a8b61a0f6e6161395248214ba6e8e
bruno.teixeira <bruno.teixeira@accenture.com>
2020-05-03 18:06:58 -0300

    Adiciona requisicao

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const request = require('request');
+const express = require('express');
 
-request('https://randomuser.me/api/?results=5', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  
-  body.results.forEach(element => {
-    console.log(element.name.first );
-  });
-  
-});
+
+function getNames()
+{
+    request('https://randomuser.me/api/?results=5', { json: true }, (err, res, body) => {
+    if (err) { return console.log(err); }
+    
+    body.results.forEach(element => {
+        element.name.first;
+        console.log(element.name.first );
+    });
+    
+    });
+}
+
+const app = express();
+
+app.get('/', (req, res) => res.send ( getNames()
+));
+
+app.listen(process.env.PORT || 3000);
+
