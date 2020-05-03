6686ca5407501004185598bcaf693c83edb166d8
bruno.teixeira <bruno.teixeira@accenture.com>
2020-05-03 18:12:26 -0300

    Apresenta valores na tela

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const request = require('request');
 const express = require('express');
 
-
 function getNames()
 {
     request('https://randomuser.me/api/?results=5', { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
-    
-    body.results.forEach(element => {
-        element.name.first;
-        console.log(element.name.first );
-    });
-    
+    return body;
     });
 }
 
 const app = express();
 
-app.get('/', (req, res) => res.send ( getNames()
+app.get('/', (req, res) => res.send ( getNames().forEach(element => {
+        element.name.first;
+        console.log(element.name.first );
+})
 ));
 
 app.listen(process.env.PORT || 3000);
